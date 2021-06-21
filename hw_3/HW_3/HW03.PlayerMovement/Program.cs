f90da2b32558b2a67e0e521d4d9bf6e73b9ffbc4
Pavel Smalenski <smolenski.p.a@gmail.com>
2021-06-21 15:48:36 +0300

    Add player movement task implementation

---
+++ hw_3/HW_3/HW03.PlayerMovement/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace HW03.PlayerMovement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char currentRotation = 'w';
+
+            while (true)
+            {
+                bool isInvalidKey = false;
+                char inputKey = char.ToLower(Console.ReadKey().KeyChar);
+
+                if (currentRotation == inputKey)
+                {
+                    Console.WriteLine(" -- rotation skipped --");
+                    continue;
+                }
+
+                switch (inputKey)
+                {
+                    case 'w':
+                        Console.WriteLine(" -- rotating UP --");
+                        break;
+                    case 'a':
+                        Console.WriteLine(" -- rotating LEFT --");
+                        break;
+                    case 's':
+                        Console.WriteLine(" -- rotating DOWN --");
+                        break;
+                    case 'd':
+                        Console.WriteLine(" -- rotating RIGHT --");
+                        break;
+                    default:
+                        isInvalidKey = true;
+                        break;
+                }
+
+                if (!isInvalidKey)
+                {
+                    currentRotation = inputKey;
+                }
+                else
+                {
+                    isInvalidKey = false;
+                }
+            }
+        }
+    }
+}
