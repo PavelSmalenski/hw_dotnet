da0f096cd87cd5a2bde5a687d0111b53aea2b3c0
Pavel Smalenski <smolenski.p.a@gmail.com>
2021-06-28 20:04:39 +0300

    Add solution for extracting arithmetic expression from string of random characters (task 1)

---
+++ hw_5/HW1.StringWithArithmetics/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+
+namespace HW1.StringWithArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your string:");
+            string input = Console.ReadLine();
+
+            int operandLeft, operandRight;
+            char arithmeticOperator;
+
+            ParseStringToArithmetics(input, out operandLeft, out operandRight, out arithmeticOperator);
+
+            Console.WriteLine($"Result of processing: {operandLeft}{arithmeticOperator}{operandRight}={CalculateExpression(operandLeft, operandRight, arithmeticOperator)}");
+        }
+
+        static void ParseStringToArithmetics(string str, out int operandLeft, out int operandRight, out char oper)
+        {
+            oper = ' ';
+
+            bool isLeftProcessed = false;
+            StringBuilder operandLeftSB = new StringBuilder("");
+            StringBuilder operandRightSB = new StringBuilder("");
+
+            foreach (var ch in str)
+            {
+                switch (ch)
+                {
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                        oper = ch;
+                        isLeftProcessed = true;
+                        continue;
+                    case char c when Char.IsDigit(ch):
+                        if (!isLeftProcessed)
+                        {
+                            operandLeftSB.Append(c);
+                        }
+                        else
+                        {
+                            operandRightSB.Append(c);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            // Using 'Parse' here to perform value validation. Either way, even user-validation would've just printed a message and threw conversion exception
+            operandLeft = int.Parse(operandLeftSB.ToString());
+            operandRight = int.Parse(operandRightSB.ToString());
+        }
+
+        static int CalculateExpression(int operandLeft, int operandRight, char oper)
+        {
+            Func<int, int, int> operFunc;
+
+            switch (oper)
+            {
+                case '+':
+                    operFunc = (x, y) => { return x + y; };
+                    break;
+                case '-':
+                    operFunc = (x, y) => { return x - y; };
+                    break;
+                case '*':
+                    operFunc = (x, y) => { return x * y; };
+                    break;
+                case '/':
+                    operFunc = (x, y) => { return x / y; };
+                    break;
+                default:
+                    throw new ArithmeticException($"Unknown operator: {oper}");
+            }
+
+            return operFunc(operandLeft, operandRight);
+        }
+    }
+}
