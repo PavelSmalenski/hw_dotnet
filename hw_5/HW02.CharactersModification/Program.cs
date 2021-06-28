8b32ec7a88c87717c3495e3d7839b1e80da0ba59
Pavel Smalenski <smolenski.p.a@gmail.com>
2021-06-28 20:35:06 +0300

    Add program to replace special characters based on Q-mark position (task 2)

---
+++ hw_5/HW02.CharactersModification/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace HW02.CharactersModification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your string:");
+            StringBuilder inputSB = new StringBuilder(Console.ReadLine());
+
+            int i = 0;
+            bool isQuestionMarkProcessed = false;
+            while (i < inputSB.Length)
+            {
+                switch (inputSB[i])
+                {
+                    case '!':
+                    case '.':
+                        inputSB.Remove(i, 1);
+                        break;
+                    case '?':
+                        isQuestionMarkProcessed = true;
+                        break;
+                    default:
+                        i++;
+                        break;
+                }
+
+                if (isQuestionMarkProcessed)
+                {
+                    break;
+                }
+            }
+
+            inputSB.Replace(' ', '_', i, inputSB.Length - i); // "Length - index" = length of a substring to the right of this index's position + char at position of index
+
+            Console.WriteLine($"Result: {inputSB.ToString()}");
+        }
+    }
+}
