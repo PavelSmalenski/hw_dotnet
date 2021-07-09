3126dde78e0f325e8879b471541cbad1a26858ce
Pavel Smalenski <smolenski.p.a@gmail.com>
2021-07-09 14:22:22 +0300

    Add solution for task 1

---
+++ hw_7/HW01.School/Control/StudentAndTeacherTest.cs
@@ -0,0 +1,30 @@
+﻿using HW01.School.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW01.School.Control
+{
+    class StudentAndTeacherTest
+    {
+        public static void PersonSayHello()
+        {
+            var person = new Person(18);
+            person.SayHello();
+        }
+
+        public static void StudentSayHello()
+        {
+            var student = new Student(21);
+            student.SayHello();
+            student.ShowAge();
+        }
+
+        public static void TeacherExplain()
+        {
+            var teacher = new Teacher(30, "Math");
+            teacher.SayHello();
+            teacher.Explain();
+        }
+    }
+}
---
+++ hw_7/HW01.School/Model/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW01.School.Model
+{
+    class Student : Person
+    {
+        public void GoToClasses()
+        {
+            Console.WriteLine("I'm going to class");
+        }
+
+        public void ShowAge()
+        {
+            Console.WriteLine($"My age is {_age}");
+        }
+
+        public Student(int age) : base(age) { }
+    }
+}
