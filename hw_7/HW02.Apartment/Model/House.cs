aa12de1f9a8a7d3879b787d4176aa90a45597e8f
Pavel Smalenski <smolenski.p.a@gmail.com>
2021-07-09 16:13:17 +0300

    Add solution for task 2

---
+++ hw_7/HW02.Apartment/Model/House.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW02.Apartment.Model
+{
+    class House
+    {
+        public int Area { get; set; }
+        public Door HouseDoor { get; set; }
+
+        public House(int area, string doorColor)
+        {
+            Area = area;
+            HouseDoor = new Door(doorColor);
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine($"I am a house, my area is {Area} m2");
+        }
+
+        public class Door
+        {
+            public string Color { get; set; }
+
+            public Door(string color)
+            {
+                Color = color;
+            }
+
+            public void ShowData()
+            {
+                Console.WriteLine($"I am a door, my color is {Color}");
+            }
+        }
+    }
+}
---
+++ hw_7/HW02.Apartment/Program.cs
@@ -0,0 +1,16 @@
+﻿using HW02.Apartment.Model;
+using System;
+
+namespace HW02.Apartment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var house = new SmallApartment("Red");
+            var person = new Person("Bob", house);
+
+            person.ShowData();
+        }
+    }
+}
