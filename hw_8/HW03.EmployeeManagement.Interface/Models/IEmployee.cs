0c6dcf0d9e381bd18f0af7c40af832e55326ac66
Pavel Smalenski <smolenski.p.a@gmail.com>
2021-07-17 21:38:13 +0300

    Add implementation of task 3 (task 1 using interface)

---
+++ hw_8/HW03.EmployeeManagement.Interface/Models/IEmployee.cs
@@ -0,0 +1,27 @@
+﻿using DataStorage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW03.EmployeeManagement.Interface.Models
+{
+    interface IEmployee
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ExperienceYears { get; set; }
+        public List<string> Responsibilities { get; set; }
+        public List<string> Technologies { get; set; }
+        public string GitURL { get; set; }
+        public int Salary { get; }
+
+        public void AddResponsibility(string responsibility);
+
+        public void AddResponsibility(IEnumerable<string> responsibility);
+
+        public void AddTechnology(string technology);
+
+        public void AddTechnology(IEnumerable<string> technology);
+    }
+}
