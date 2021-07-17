adf014318d412e01274176ece10a60e1b0b341cb
Pavel Smalenski <smolenski.p.a@gmail.com>
2021-07-17 21:14:38 +0300

    Add implementation of task 2 (task 1 via abstract class)

---
+++ hw_8/HW02.EmployeeManagement.Abstract/Models/Employee.cs
@@ -0,0 +1,53 @@
+﻿using DataStorage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW02.EmployeeManagement.Abstract.Models
+{
+    abstract class Employee
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int ExperienceYears { get; private set; }
+        public List<string> Responsibilities { get; private set; } = new List<string>();
+        public List<string> Technologies { get; private set; } = new List<string>();
+        public string GitURL { get; private set; }
+        public abstract int Salary { get; }
+
+        public Employee(string firstName, string lastName, int experienceYears, string gitURL)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            ExperienceYears = experienceYears;
+            GitURL = gitURL;
+            AddResponsibility(EmployeeConstants.Responsiblities.Common);
+        }
+
+        public void AddResponsibility(string responsibility)
+        {
+            Responsibilities.Add(responsibility);
+        }
+
+        public void AddResponsibility(IEnumerable<string> responsibility)
+        {
+            Responsibilities.AddRange(responsibility);
+        }
+
+        public void AddTechnology(string technology)
+        {
+            Technologies.Add(technology);
+        }
+
+        public void AddTechnology(IEnumerable<string> technology)
+        {
+            Technologies.AddRange(technology);
+        }
+
+        public override string ToString()
+        {
+            return $"Full Name: {FirstName} {LastName}, Experience: {ExperienceYears}, Title: {this.GetType().Name}, Salary: {Salary}, GitHub: {GitURL}";
+        }
+    }
+}
---
+++ hw_8/HW02.EmployeeManagement.Abstract/Program.cs
@@ -0,0 +1,23 @@
+﻿using HW02.EmployeeManagement.Abstract.Controls;
+using HW02.EmployeeManagement.Abstract.Models.Developers;
+using System;
+
+namespace HW02.EmployeeManagement.Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Note: well... nothing really changed except for Employee becoming Abstract and Salary property too
+            var employeeManagement = new Controls.EmployeeManagement("Company_Name");
+
+            employeeManagement.AddEmployee(new Junior("Name1", "Surname1", 1, "https://git.com/someGit1"));
+            employeeManagement.AddEmployee(new Middle("Name2", "Surname2", 1, "https://git.com/someGit2"));
+            employeeManagement.AddEmployee(new Senior("Name3", "Surname3", 1, "https://git.com/someGit3"));
+            employeeManagement.AddEmployee(new TechLead("Name4", "Surname4", 1, "https://git.com/someGit4"));
+            employeeManagement.AddEmployee(new Architect("Name5", "Surname5", 1, "https://git.com/someGit5"));
+
+            employeeManagement.PrintEmployee();
+        }
+    }
+}
