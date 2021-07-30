562eb9b66dc1f04d4cf931bc9652817631ea7a5e
Pavel Smalenski <smolenski.p.a@gmail.com>
2021-07-30 23:20:20 +0300

    Add task 2 implementation - custom queues

---
+++ hw_13/HW02.CustomQueue/Controls/QueueBasics.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW02.CustomQueue.Controls
+{
+    abstract class QueueBasics<T>
+    {
+        private class QueueItem<T>
+        {
+            public QueueItem<T> Prev { get; set; } = null;
+            public T Item { get; set; }
+
+            public QueueItem(T item, QueueItem<T> prev)
+            {
+                Item = item;
+                Prev = prev;
+            }
+        }
+
+        QueueItem<T> _head = null;
+
+        public T Dequeue()
+        {
+            QueueItem<T> result = _head;
+            if (!(_head is null))
+            {
+                _head = _head.Prev;
+                return result.Item;
+            }
+            else
+            {
+                // Not the brightest idea to return default(T) if queue is empty, but
+                //   1) "T?" usage was restricted for REF types only and this class was meant to be "all in one"
+                //   2) "A nullable type parameter must be known to be a value type or
+                //      non-nullable reference type unless language version '9.0' or greater is used" - that's what compiler said
+                return default(T);
+            }
+        }
+
+        public void Enqueue(T item)
+        {
+            var newItem = new QueueItem<T>(item, _head);
+            _head = newItem;
+        }
+
+        public T Peek()
+        {
+            if (!(_head is null))
+            {
+                return _head.Item;
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            var pointer = _head;
+
+            while (!(pointer is null))
+            {
+                yield return pointer.Item is null ? "null" : pointer.Item.ToString();
+                pointer = pointer.Prev;
+            }
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            foreach (var item in this)
+            {
+                result += $"=>{item}";
+            }
+            return result;
+        }
+    }
+}
---
+++ hw_13/HW02.CustomQueue/Program.cs
@@ -0,0 +1,58 @@
+﻿using HW02.CustomQueue.Controls;
+using System;
+
+namespace HW02.CustomQueue
+{
+    class Program
+    {
+        class TestObject
+        {
+            Guid Id { get; } = Guid.NewGuid();
+
+            public override string ToString()
+            {
+                return Id.ToString();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var queueDateTime = new QueueDateTime();
+            queueDateTime.Enqueue(new DateTime(2001, 01, 01));
+            queueDateTime.Enqueue(new DateTime(2002, 02, 02));
+            queueDateTime.Enqueue(new DateTime(2003, 03, 03));
+            queueDateTime.Enqueue(new DateTime(2004, 04, 04));
+            queueDateTime.Enqueue(new DateTime(2005, 05, 05));
+            TestQueue(queueDateTime);
+
+            var queueObject = new QueueObject();
+            queueObject.Enqueue("abc");
+            queueObject.Enqueue(123);
+            queueObject.Enqueue(null);
+            queueObject.Enqueue('c');
+            queueObject.Enqueue(111.11);
+            TestQueue(queueObject);
+
+            var queueGeneric = new QueueGeneric<TestObject>();
+            queueGeneric.Enqueue(new TestObject());
+            queueGeneric.Enqueue(new TestObject());
+            queueGeneric.Enqueue(new TestObject());
+            queueGeneric.Enqueue(new TestObject());
+            queueGeneric.Enqueue(new TestObject());
+            TestQueue(queueGeneric);
+        }
+
+        static void TestQueue<T>(QueueBasics<T> queue)
+        {
+            Console.WriteLine($"Full queue: {queue}");
+
+            var item = queue.Peek();
+            Console.WriteLine($"Queue after Peek: {queue}");
+            Console.WriteLine($"Peeked item: {item}");
+
+            item = queue.Dequeue();
+            Console.WriteLine($"Queue after Dequeue: {queue}");
+            Console.WriteLine($"Dequeued item: {item}");
+        }
+    }
+}
