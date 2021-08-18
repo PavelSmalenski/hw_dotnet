221d293a3778d6c8530e9158484c7ab32afb465b
Pavel Smalenski <smolenski.p.a@gmail.com>
2021-08-18 18:18:38 +0300

    Add external project Motoshop with two new attributes for Guid and MinYear

---
+++ Motoshop/Motoshop/Models/Moto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Motoshop.Models.Attributes;
+
+namespace Motoshop.Models
+{
+    public class Moto
+    {
+        [Guid(ErrorMessage = "Guid format mismatch")]
+        public Guid Id { get; set; }
+        [Required]
+        public int Odometer { get; set; }
+        public string Model { get; set; }
+        [MinYear(2000, ErrorMessage = "Year can not be less than 2000")]
+        public DateTime MadeIn { get; set; }
+        public string ImagePreview { get; set; }
+    }
+}
